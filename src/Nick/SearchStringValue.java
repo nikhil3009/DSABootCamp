3077041990c402d0a519c9943352b011f1758d8d
pr612j <pr612j@att.com>
2022-02-26 00:41:21 +0530

    search program

---
+++ src/Nick/SearchStringValue.java
@@ -0,0 +1,22 @@
+package Nick;
+
+public class SearchStringValue {
+    public static void main(String[] args) {
+        String str  = "sainikhil";
+        char target = 'z';
+        System.out.println(search(str,target));
+
+    }
+    static boolean search(String str, char target){
+        if(str.length()==0){
+            return false;
+        }
+        for(int i=0; i<str.length();i++){
+            if(target == str.charAt(i)){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
