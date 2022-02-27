a9131dbf2758e48e6526905685b811bb7c632e56
pr612j <sainikhil3009@gmail.com>
2022-02-28 02:07:02 +0530

    day4

---
+++ src/Binary/Floor.java
@@ -0,0 +1,42 @@
+package Binary;
+
+public class Floor {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 7, 8, 14, 16, 18, 20};
+        int target = 15;
+        int ans = searchFloor(arr, target);
+        System.out.println(ans);
+    }
+    static int searchFloor(int[] arr, int target)
+        {
+            int start = 0;
+            int end = (arr.length)-1;
+            boolean asc = arr[start]<arr[end];
+            while(start<=end){
+                int mid = start +(end-start)/2;
+                if(target == arr[mid]){
+                    return mid;
+                }
+                if(asc){
+                    if(target < arr[mid]){
+                        end = mid-1;
+                    }
+                    else
+                    {
+                        start = mid+1;
+                    }
+                }
+                else{
+                    if(target < arr[mid]){
+                        start = mid+1;
+                    }
+                    else{
+                        end = mid-1;
+                    }
+                }
+            }
+            return end;
+
+        }
+    }
+
