a1ee598650dabcddd97d932733fa448310f0ded7
pr612j <sainikhil3009@gmail.com>
2022-03-10 23:58:14 +0530

    day11

---
+++ src/sorting/cyclicSort/DisappearedNumbers.java
@@ -0,0 +1,37 @@
+package sorting.cyclicSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DisappearedNumbers {
+    public static void main(String[] args) {
+        int[] arr = {4,1,3,3,2,2,7,8};
+        List<Integer> ans = search(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static List<Integer> search(int[] arr){
+        int i =0;
+        while(i< arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index<arr.length;index++){
+            if(arr[index]!= (index+1)){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[second];
+        arr[second] = arr[first];
+        arr[first] = temp;
+    }
+}
