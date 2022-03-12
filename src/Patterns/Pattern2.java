1e97d9bcebd3a8ed6f13e20522f2d9a0c4ee7c92
pr612j <sainikhil3009@gmail.com>
2022-03-12 21:51:30 +0530

    day13

---
+++ src/Patterns/Pattern2.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        patern(4);
+    }
+    static void patern(int n){
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
