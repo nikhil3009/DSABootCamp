c156ceba537511eadbf33b3822e5eed434aec66f
pr612j <sainikhil3009@gmail.com>
2022-03-17 16:48:30 +0530

    day17

--- src/MathProblems/UniqueElement.java
+++ src/MathProblems/UniqueElement.java
@@ -2,12 +2,15 @@ package MathProblems;
 
 public class UniqueElement {
     public static void main(String[] args) {
-        int[] arr = {2,4,6,2,4,6,8};
+        int[] arr = {-2,4,6,2,-4,6,8};
         findUnique(arr);
     }
     static void findUnique(int[] arr){
         int unique = 0;
         for(int n : arr){
+            if(n<0){
+                n= n*(-1);
+            }
             unique ^= n;
         }
         System.out.println(unique);
