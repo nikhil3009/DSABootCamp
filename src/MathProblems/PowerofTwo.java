4fccff9aeb230f51bbf848971b00abb4856bd424
pr612j <sainikhil3009@gmail.com>
2022-03-18 13:33:40 +0530

    day18

--- src/MathProblems/PowerofTwo.java
+++ src/MathProblems/PowerofTwo.java
@@ -2,9 +2,14 @@ package MathProblems;
 
 public class PowerofTwo {
     public static void main(String[] args) {
-        int n = 256;
-        boolean ans = (n & (n-1)) == 0;
-        System.out.println(ans);
+        int n = 4;
+        if(n==0){
+            System.out.println("false");
+        }
+        else {
+            boolean ans = (n & (n - 1)) == 0;
+            System.out.println(ans);
+        }
     }
 
 }
---
+++ src/MathProblems/SetBits.java
@@ -0,0 +1,16 @@
+package MathProblems;
+
+public class SetBits {
+    public static void main(String[] args) {
+        int n = 31;
+        int count = 0;
+        while(n>0){
+            if((n&1) == 1){
+                count++;
+            }
+            n = n>>1;
+        }
+        System.out.println(count);
+    }
+
+}
