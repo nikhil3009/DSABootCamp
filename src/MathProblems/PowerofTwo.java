440ea8bde647faeb1ac517f6913e45913d24cb64
pr612j <sainikhil3009@gmail.com>
2022-03-18 13:21:33 +0530

    day18

--- src/MathProblems/PowerofTwo.java
+++ src/MathProblems/PowerofTwo.java
@@ -1,5 +1,10 @@
 package MathProblems;
 
 public class PowerofTwo {
+    public static void main(String[] args) {
+        int n = 256;
+        boolean ans = (n & (n-1)) == 0;
+        System.out.println(ans);
+    }
 
 }
