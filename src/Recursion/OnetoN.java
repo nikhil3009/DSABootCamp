3f609197a520e2e9eb697d863db7852c03260502
pr612j <sainikhil3009@gmail.com>
2022-03-23 19:06:30 +0530

    day22

--- src/Recursion/OnetoN.java
+++ src/Recursion/OnetoN.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class OnetoN {
     public static void main(String[] args) {
-        int n=10;
+        int n=100;
         printN(n);
     }
     static void printN(int n){
