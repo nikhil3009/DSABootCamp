92a0503d32566b773b945f8529e1781ff04cd787
pr612j <sainikhil3009@gmail.com>
2022-03-26 23:40:13 +0530

    day24

---
+++ src/Recursion/Pattern2.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        triangle(4,0);
+    }
+    static void triangle(int r, int c){
+        if(r==0){
+            return;
+        }
+        if(r>c){
+            triangle(r,c+1);
+            System.out.print("* ");
+
+        }else {
+            triangle(r - 1, 0);
+            System.out.println();
+
+        }
+    }
+}
+
